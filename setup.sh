7a4a4a2847a8e7ca23bfed3e9566e3d244385cd3
jean <thierryscully.colomban@gmail.com>
2021-04-30 07:44:36 +0200

    second commit

--- setup.sh
+++ setup.sh
@@ -7,12 +7,15 @@ minikube dashboard &
 
 eval $(minikube docker-env)
 
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
 IP=$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)
 printf "minikube IP: ${IP}"
 docker build -t service_nginx ./srcs/nginx
+docker build -t service_mysql ./srcs/mysql
+docker build -t service_php-my-admin ./srcs/phpmyadmin
 
-kubectl create -f ./srcs/
-
-open http://$IP
-
-
+kubectl apply -f ./srcs/
